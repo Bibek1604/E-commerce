b8ca9f3659a6005dbaf6b28ae435e84a795e6445
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-18 00:42:15 +0545

    data added in database

--- E-commerce/myproject/myapp/views.py
+++ E-commerce/myproject/myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from product.models import product
+
 
 # Create your views here.
 def frontpage(request):
-    return render (request, 'frontpage.html')
+    products=product.objects.all()[0:8]
+    return render (request, 'frontpage.html',{'products':products})

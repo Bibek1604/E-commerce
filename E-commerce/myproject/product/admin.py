bbee67b29086ab8d5953c1787bcdf72e1d8a8d41
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-18 00:34:51 +0545

    categories added

--- E-commerce/myproject/product/admin.py
+++ E-commerce/myproject/product/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category
+from .models import Category, product
 admin.site.register(Category)
+admin.site.register(product)

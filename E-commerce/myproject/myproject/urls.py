26f494af741b874484e069dbda4e0dc956b4b4b2
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-19 18:28:38 +0545

    proudct page -logic added

--- E-commerce/myproject/myproject/urls.py
+++ E-commerce/myproject/myproject/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from myapp.views import frontpage, shop
+from myapp.views import frontpage, shop_view
 from product.views import product_view
 from product.models import Product, Category
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', frontpage, name='frontpage'),
-    path('shop/<slug:slug>/', shop, name='shop'),  # Removed extra space
+    path('shop/<slug:slug>/', shop_view, name='shop'),
     path('product/', product_view, name='product'),
 ]

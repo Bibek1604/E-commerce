9d685f52e1788d1dcd4bdb5d619d39f25633bc6f
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-20 20:58:06 +0545

    all the code changes

--- E-commerce/myproject/myapp/views.py
+++ E-commerce/myproject/myapp/views.py
@@ -1,36 +1,31 @@
-from django.shortcuts import render
 from django.db.models import Q
-from product.models import Category, Product  # Corrected import
+from django.shortcuts import render
 
-def get_categories():
-    return Category.objects.all()
+from product.models import Product, Category
 
 def frontpage(request):
-    products =Product.objects.all()[0:8]
-    # context = {'categories': categories}
-    return render(request, 'frontpage.html', {'products':products})
+    products = Product.objects.all()[0:8]
 
-def shop(request, slug=None):  # Updated function signature
-    categories = get_categories()
+    return render(request, 'core/frontpage.html', {'products': products})
+
+def shop(request):
+    categories = Category.objects.all()
     products = Product.objects.all()
-    
-    active_category_slug = request.GET.get('category', '')
-    if active_category_slug:
-        products = products.filter(category__slug=active_category_slug)
-        
+
+    active_category = request.GET.get('category', '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
     query = request.GET.get('query', '')
+
     if query:
         products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
-    
+
     context = {
         'categories': categories,
         'products': products,
-        'active_category_slug': active_category_slug,
+        'active_category': active_category
     }
-    
-    return render(request, 'shop.html', context)
 
-def product_view(request, slug):
-    product = Product.objects.get(slug=slug)
-    context = {'product': product}
-    return render(request, 'product.html', context)
+    return render(request, 'core/shop.html', context)
--- E-commerce/myproject/product/models.py
+++ E-commerce/myproject/product/models.py
@@ -1,28 +1,8 @@
-from django.db import models
+from django.shortcuts import render, get_object_or_404
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    slug = models.SlugField()
+from .models import Product
 
-    class Meta:
-        ordering = ('name',)
+def product(request, slug):
+    product = get_object_or_404(Product, slug=slug)
 
-    def __str__(self):
-        return self.name
-
-class Product(models.Model):
-    category = models.ForeignKey(Category,related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    slug = models.SlugField()
-    description = models.TextField(blank=True, null=True)
-    price = models.IntegerField()  # Assuming price is a decimal value
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ('-created_at',)
-
-    def __str__(self):
-        return self.name
-
-    def get_display_price(self):
-        return self.price  # Adjusted to return the actual price without division
+    return render(request, 'product/product.html', {'product': product})

89b5d6955b6ce580fd95a40b215510d21cf2e950
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-04-20 12:46:44 +0545

    slug and product not working

--- E-commerce/myproject/myproject/urls.py
+++ E-commerce/myproject/myproject/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from myapp.views import frontpage, shop  # Updated import statement
+from myapp.views import frontpage, shop, product_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', frontpage, name='frontpage'),
-    path('shop/<slug>/', shop, name='shop'),
-  # Updated path to use 'shop' instead of 'shop_view'
+    path('shop/', shop, name='shop'),
+    path('shop/<slug:slug>/', product_view, name='product'),  # Assuming 'product_view' is your view function for the product page
+    path('admin/', admin.site.urls),
 ]
